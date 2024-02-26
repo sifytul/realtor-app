09a4f6312cdedc4e263dadbf5402f951a24f6797
Sifytul Karim <69572135+sifytul@users.noreply.github.com>
2024-02-26 21:25:47 +0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,10 +1,9 @@
 def buildApp() {
     echo 'building...'
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        // sh 'docker build -t sifytul/demo-app:realtor-2.0 .'
-        // sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
-        // sh 'docker push sifytul/demo-app:realtor-2.0'
-        sh 'echo $USERNAME'
+        sh 'docker build -t sifytul/demo-app:realtor-2.0 .'
+        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+        sh 'docker push sifytul/demo-app:realtor-2.0'
     }
 }
 
